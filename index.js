d2934f6fbe943ba16a7d5beee3ca602ecb6c085a
Faith Muraya <111008831+faith-muraya@users.noreply.github.com>
2023-09-21 16:46:02 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+function returnFirstTwoDrivers(){
+    return drivers.slice(0, 2);
+}
+
+function returnLastTwoDrivers(){
+    return drivers.slice(-2);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(int) {
+    return (fare) => {
+        return fare * int;
+    };
+}
+const fareDoubler = createFareMultiplier(2)
+const fareTripler = createFareMultiplier(3);   
+
+const selectDifferentDrivers = (arrayOfDrivers, functionReturnDrivers) => {
+    if (functionReturnDrivers === returnFirstTwoDrivers) {
+      return arrayOfDrivers.slice(0, 2);
+    }
+    return arrayOfDrivers.slice(-2);
+  };
